ffb6684080c4969ce15abc239e66dcbe3a73d6d2
satsuralala <satsuraltumurbat@gmail.com>
2024-11-12 16:49:25 +0800

    fourth

--- graphql/resolvers/mutations/add-task.ts
+++ graphql/resolvers/mutations/add-task.ts
@@ -1,25 +1,16 @@
 import { Task } from "@/graphql/models/Tasks";
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
 export const addTask = async (
   _: unknown,
   { taskName, priority }: { taskName: string; priority: number }
 ) => {
   if (!taskName) {
-<<<<<<< Updated upstream
     throw new Error("task cannot be empty");
-=======
-    throw new Error(" taskName cannot be empty");
->>>>>>> Stashed changes
   }
   try {
     const newTask = await Task.create({
       taskName,
       isDone: false,
-<<<<<<< Updated upstream
       priority,
       createdAt: new Date(),
     });
@@ -28,25 +19,3 @@ export const addTask = async (
     console.log(error);
   }
 };
-=======
-export const addTask = async (_: unknown, { taskName, priority }: { taskName: string, priority:number }) => {
-  try{
-    const newTask=await Task.create(
-      {taskName,
-      isDone:false,
-=======
->>>>>>> Stashed changes
-      priority,
-      createdAt: new Date(),
-    });
-    return newTask;
-  } catch (error) {
-    console.log(error);
-  }
-};
-<<<<<<< Updated upstream
-
-
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
--- specs/mutations/add-task.spec.ts
+++ specs/mutations/add-task.spec.ts
@@ -9,75 +9,33 @@ jest.mock("../../graphql/models/Tasks",()=>({
             isDone:false,
             createdAt:new Date(),
             updatedAt:new Date(),
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         }).mockRejectedValueOnce(new Error("failed to add a task"))
         .mockRejectedValueOnce(new Error("task cannot be empty"))
-=======
-        }).mockRejectedValueOnce(new Error("error happened during adding the task"))
->>>>>>> Stashed changes
-=======
-        }).mockRejectedValueOnce(new Error("failed to add a task"))
-        .mockRejectedValueOnce(new Error("task cannot be empty"))
->>>>>>> Stashed changes
     },
 }));
 
 describe("Adding task mutation",()=>{
     it("Should add a task successfully", async()=>{
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         const res=await addTask({},{taskName:"task1", priority:8});
         expect(res).toHaveProperty("taskName", "task1");
         expect(res).toHaveProperty("priority", 8);
         expect(res.isDone).toStrictEqual(false);
-=======
-        const res=await addTask({},{taskName:"test task", priority:9});
-        expect(res).toHaveProperty("taskName", "task1");
-        expect(res).toHaveProperty("priority", 8);
-        expect(res.isDone).toEqual(false);
->>>>>>> Stashed changes
-=======
-        const res=await addTask({},{taskName:"task1", priority:8});
-        expect(res).toHaveProperty("taskName", "task1");
-        expect(res).toHaveProperty("priority", 8);
-        expect(res.isDone).toStrictEqual(false);
->>>>>>> Stashed changes
     })
 
     it("Should throw an error when task creation fails", async () => {
         try {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
             await addTask({}, { taskName: "task2", priority: 1 });
         } catch (error: unknown) {  
             if (error instanceof Error) { 
                 expect(error.message).toBe("failed to add a task");
-=======
-            await addTask({}, { taskName: "test task", priority: 8 });
-        } catch (error: unknown) {  
-            if (error instanceof Error) { 
-                expect(error.message).toBe("Error during task creation");
->>>>>>> Stashed changes
-=======
-            await addTask({}, { taskName: "task2", priority: 1 });
-        } catch (error: unknown) {  
-            if (error instanceof Error) { 
-                expect(error.message).toBe("failed to add a task");
->>>>>>> Stashed changes
             } else {
                 throw error; 
             }
             
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
         }        
     });
 
     it("Should throw an error when  task is empty", async()=>{
-<<<<<<< Updated upstream
         try {
             await addTask({}, { taskName: "", priority: 8 });
         } catch (error) {  
@@ -88,25 +46,6 @@ describe("Adding task mutation",()=>{
             }
         }
 
-=======
-        }
-    
-=======
->>>>>>> Stashed changes
-        try {
-            await addTask({}, { taskName: "", priority: 8 });
-        } catch (error) {  
-            if(error instanceof Error){
-                expect(error.message).toBe("task cannot be empty")
-            }else{
-                throw error;
-            }
-        }
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
-
->>>>>>> Stashed changes
     });
     
     
